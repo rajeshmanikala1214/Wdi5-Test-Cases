2b3e156a42b1defccb5af6c838b5fb13c4179b5d
rajeshmanikala1214 <rajesh.manikala812@gmail.com>
2026-03-26 15:51:17 +0530

    added new wdio.conf.js file

---
+++ webapp/test/wdio.conf.js
@@ -0,0 +1,37 @@
+exports.config = {
+    runner: 'local',
+
+    specs: [
+        './webapp/test/e2e/*.test.js'
+    ],
+
+    maxInstances: 1,
+
+    capabilities: [{
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage']
+        }
+    }],
+
+    services: ['chromedriver', 'ui5'],
+
+    framework: 'mocha',
+
+    reporters: [
+        'spec',
+        ['junit', {
+            outputDir: './reports/junit',
+            outputFileFormat: function (opts) {
+                return `results-${opts.cid}.xml`;
+            }
+        }]
+    ],
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+
+    baseUrl: 'http://localhost:8080'
+};

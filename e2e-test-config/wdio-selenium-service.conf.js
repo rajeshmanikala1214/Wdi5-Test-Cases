ff171b2cdeeb027cd78879175680849983ff05bf
rajeshmanikala1214 <rajesh.manikala812@gmail.com>
2026-03-26 15:38:23 +0530

    WDI5 working project

---
+++ e2e-test-config/wdio-selenium-service.conf.js
@@ -0,0 +1,12 @@
+const { baseConfig } = require("./wdio.base.conf")
+
+const _config = {
+    specs: ["../webapp/test/e2e/**/*.test.js"],
+    exclude: ["../webapp/test/e2e/ui5-late.test.js"],
+    logLevel: "error",
+    bail: 0,
+    baseUrl: "http://localhost:8888",
+    services: [["selenium-standalone", { drivers: { chrome: true, chromiumedge: "latest" } }]]
+}
+
+exports.config = { ...baseConfig, ..._config }
---
+++ webapp/test/wdio.base.conf.js
@@ -0,0 +1,44 @@
+const { join } = require("node:path")
+const { cpus } = require("node:os")
+const maxInstances = Math.max(1, Math.floor(cpus().length / 2))
+
+exports.baseConfig = {
+    wdi5: {
+        screenshotPath: join("webapp", "test", "__screenshots__"),
+        logLevel: "error",
+        waitForUI5Timeout: 29000
+    },
+    maxInstances: maxInstances,
+    capabilities: [
+        {
+            maxInstances: maxInstances,
+            browserName: "chrome",
+            // browserVersion: "stable",
+            acceptInsecureCerts: true,
+            // "wdio:enforceWebDriverClassic": true,
+            "goog:chromeOptions": {
+                args: process.argv.includes("--headless")
+                    ? ["window-size=1920,1280", "headless", "disable-gpu"]
+                    : process.argv.includes("--debug")
+                      ? ["window-size=1920,1280", "auto-open-devtools-for-tabs", "--start-fullscreen"]
+                      : ["window-size=1920,1280"]
+            }
+        }
+    ],
+    logLevel: "error",
+    bail: 0,
+    baseUrl: "http://localhost:8081/index.html",
+
+    waitforTimeout: 30000,
+    connectionRetryTimeout: process.argv.includes("--debug") ? 1200000 : 120000,
+    connectionRetryCount: 3,
+
+    services: ["ui5"],
+
+    framework: "mocha",
+    mochaOpts: {
+        ui: "bdd",
+        timeout: process.argv.includes("--debug") ? 600000 : 90000
+    },
+    reporters: ["spec"]
+}

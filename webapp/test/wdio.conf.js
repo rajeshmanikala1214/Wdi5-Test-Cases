62f3abfca1d74cc4f77e53d34b01a460cd1522c2
rajeshmanikala1214 <rajesh.manikala812@gmail.com>
2026-03-26 16:08:26 +0530

    changes in package.json and conf.js files

--- webapp/test/wdio.conf.js
+++ webapp/test/wdio.conf.js
@@ -1,52 +1,25 @@
 exports.config = {
     runner: 'local',
-
-    specs: [
-        './webapp/test/e2e/*.test.js'
-    ],
-
+    specs: ['./webapp/test/e2e/*.test.js'],
     maxInstances: 1,
-
     capabilities: [{
         browserName: 'chrome',
         'goog:chromeOptions': {
-            args: [
-                '--headless',
-                '--no-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-gpu',
-                '--window-size=1920,1080'
-            ]
+            args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage']
         }
     }],
-
-    // Use the chromedriver service (works in Docker/CI)
-    services: [
-        ['chromedriver'],
-        'ui5'
-    ],
-
+    services: ['chromedriver', 'ui5'],
     framework: 'mocha',
-
     reporters: [
         'spec',
         ['junit', {
             outputDir: './reports/junit',
-            outputFileFormat: function (opts) {
-                return `results-${opts.cid}.xml`;
-            }
+            outputFileFormat: opts => `results-${opts.cid}.xml`
         }]
     ],
-
     mochaOpts: {
         ui: 'bdd',
-        timeout: 120000 // 2 minutes per test
+        timeout: 60000
     },
-
-    baseUrl: 'http://localhost:8080',
-
-    // Retry failed tests in CI
-    specFileRetries: 1,
-    specFileRetriesDelay: 3,
-    specFileRetriesDeferred: false
+    baseUrl: 'http://localhost:8080'
 };
